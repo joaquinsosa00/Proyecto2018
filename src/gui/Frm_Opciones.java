5fb7c4d3b13b1f88486d66b447b9b254af51b200
joaquin <joaquin@LAPTOP-ESJHCE4Q>
2019-02-24 18:34:57 -0300

    ultimo commit andando todo

--- src/gui/Frm_Opciones.java
+++ src/gui/Frm_Opciones.java
@@ -6,8 +6,11 @@
 package gui;
 
 import java.awt.Image;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -23,8 +26,15 @@ public class Frm_Opciones extends javax.swing.JFrame {
         ImageIcon nuevo = new ImageIcon (getClass().getResource("/imagen/unt.png"));
         ImageIcon icono = new ImageIcon(nuevo.getImage().getScaledInstance(jLabel1.getWidth(), jLabel1.getHeight(), Image.SCALE_DEFAULT));
         jLabel1.setIcon(icono);
+        ImageIcon nuevo1 = new ImageIcon (getClass().getResource("/imagen/fondonaranja2.jpg"));
+        ImageIcon icono1 = new ImageIcon(nuevo1.getImage().getScaledInstance(jLabel2.getWidth(), jLabel2.getHeight(), Image.SCALE_DEFAULT));
+        jLabel2.setIcon(icono1);
+        this.setLocationRelativeTo(this);
+        cerrar();
         
-        setIconImage(new ImageIcon(getClass().getResource("/imagen/icono ventana.png")).getImage() );
+        
+        
+        setIconImage(new ImageIcon(getClass().getResource("/imagen/birrete (2).png")).getImage() );
     }
 
     /**
@@ -38,68 +48,53 @@ public class Frm_Opciones extends javax.swing.JFrame {
 
         btnGuardado = new javax.swing.JButton();
         btnLista = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
         btnSalir = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Trabajo");
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        btnGuardado.setBackground(new java.awt.Color(0, 102, 102));
+        btnGuardado.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        btnGuardado.setForeground(new java.awt.Color(240, 240, 240));
         btnGuardado.setText("Trabajo nuevo");
         btnGuardado.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnGuardadoActionPerformed(evt);
             }
         });
+        getContentPane().add(btnGuardado, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 180, 170, -1));
 
+        btnLista.setBackground(new java.awt.Color(0, 102, 102));
+        btnLista.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        btnLista.setForeground(new java.awt.Color(240, 240, 240));
         btnLista.setText("Lista de trabajos");
         btnLista.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnListaActionPerformed(evt);
             }
         });
+        getContentPane().add(btnLista, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 180, 200, -1));
 
+        btnSalir.setBackground(new java.awt.Color(153, 0, 0));
+        btnSalir.setFont(new java.awt.Font("Trebuchet MS", 3, 14)); // NOI18N
+        btnSalir.setForeground(new java.awt.Color(240, 240, 240));
         btnSalir.setText("Salir");
         btnSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnSalirActionPerformed(evt);
             }
         });
+        getContentPane().add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 270, 87, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 540, 150));
 
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(81, 81, 81)
-                .addComponent(btnGuardado, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(82, 82, 82)
-                .addComponent(btnLista, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(97, 97, 97)
-                .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 278, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(82, 82, 82)
-                .addComponent(btnGuardado)
-                .addGap(18, 18, 18)
-                .addComponent(btnLista)
-                .addGap(18, 18, 18)
-                .addComponent(btnSalir))
-            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
+        jLabel3.setBackground(new java.awt.Color(0, 0, 0));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 540, 150));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 4, 560, 380));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -112,10 +107,14 @@ public class Frm_Opciones extends javax.swing.JFrame {
     }//GEN-LAST:event_btnGuardadoActionPerformed
 
     private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
-        
-        System.exit(0);
+         
+       int exit = JOptionPane.showConfirmDialog(null, "Desea salir?", "Exit", JOptionPane.YES_NO_OPTION);
+       if (exit == JOptionPane.YES_OPTION){
+           System.exit(0);
+       }
     }//GEN-LAST:event_btnSalirActionPerformed
 
+    
     private void btnListaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListaActionPerformed
         // TODO add your handling code here:
         JFrame frame = new Frm_ListaTrabajos();
@@ -124,11 +123,35 @@ public class Frm_Opciones extends javax.swing.JFrame {
             this.setVisible(false);
     }//GEN-LAST:event_btnListaActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        jButton1.setVisible(false);
-    }//GEN-LAST:event_jButton1ActionPerformed
-
+    public void cerrar(){
+        try {
+          this.setDefaultCloseOperation(Frm_Opciones.DO_NOTHING_ON_CLOSE);
+            addWindowListener(new WindowAdapter(){
+            public void windowClosing (WindowEvent e){
+                confirmarSalida();
+                
+//            frame1.setVisible(false);
+            
+            }
+        });
+        this.setVisible(true);
+        }
+        catch(Exception e){
+        e.printStackTrace();
+    }
+    }
+    public void confirmarSalida(){
+        int valor= JOptionPane.showConfirmDialog(null, "Esta seguro que desea salir?","ADVERTENCIA",JOptionPane.YES_NO_OPTION);
+        
+        if (valor==JOptionPane.YES_OPTION){ 
+//            this.setVisible(false);
+//            JFrame frame = new Frm_Opciones();
+//               
+//            frame.setLocationRelativeTo(frame);
+//            frame.setVisible(true);
+            System.exit(0);
+        }
+    }
     /**
      * @param args the command line arguments
      */
@@ -168,7 +191,8 @@ public class Frm_Opciones extends javax.swing.JFrame {
     private javax.swing.JButton btnGuardado;
     private javax.swing.JButton btnLista;
     private javax.swing.JButton btnSalir;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
